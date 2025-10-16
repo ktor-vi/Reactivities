32084f3307f8cd0c23a5d9d9d3d9ddbc02d1e247
Victor Philippe <me@ktorvi.me>
2025-10-16 03:10:09 +0200

    end of section 3

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { List, ListItemText, ListItem, Typography } from "@mui/material";
+import axios from "axios";
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  useEffect(() => {
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+    .then(response => setActivities(response.data))
+    return () => {}
+  }, [])
+  return (
+    <>
+      <Typography variant="h3">Reactivities</Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App
